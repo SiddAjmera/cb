0b4f6620958738c06a1b73243b36e8baa58bc977
unknown <962060@01HW507669.India.TCS.com>
2015-12-30 19:02:20 +0530

    Profile Page

--- client/app/userProfile/userProfile.controller.js
+++ client/app/userProfile/userProfile.controller.js
@@ -1,6 +1,32 @@
 'use strict';
 
 angular.module('cbApp')
-  .controller('UserProfileCtrl', function ($scope) {
+  .controller('UserProfileCtrl', function ($scope, $modal, cordovaUtil) {
     $scope.message = 'Hello';
+
+
+    $scope.getLocation=function(){
+         var modalInstance = $modal.open({
+          animation: true,
+          templateUrl: 'components/modal/modal.html',
+          controller: 'ModalCtrl',
+          size: 'sm'
+        });
+        
+        modalInstance.result.then(function(option){
+          if(option == "yes")
+          cordovaUtil.getUserHomeCoordinates().then(function(address){
+          	alert('Promise was returned successfully. Address is : ' + address);
+           	$scope.user.homeAddress=address.homeAddress;
+            $scope.user.city=address.city;
+            $scope.user.zipcode=address.zipcode;
+            $scope.user.placeID=address.placeID;
+          })
+        });
+    };
+
+    $scope.syncUserLocationData = function(){
+    	cordovaUtil.syncCoordinates();
+    };
+
   });
