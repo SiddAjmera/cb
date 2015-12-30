6882ae403e6cc92b93d38f4a3ab3adf4b0f5d6e6
MD ASHRAF <md.ashraf2@tcs.com>
2015-12-30 17:36:39 +0530

    map icon changes

--- client/app/suggestions/suggestions.controller.js
+++ client/app/suggestions/suggestions.controller.js
@@ -1,14 +1,48 @@
 'use strict';
 
 angular.module('cbApp')
-  .controller('SuggestionsCtrl', function ($scope) {
+  .controller('SuggestionsCtrl', function ($scope, leafletMarkerEvents) {
+   
+        
+
+
+    $scope.markers= [];
     $scope.center={
-        lat: 51.505,
-        lng: 10.75,
-        zoom: 8
-    }
-    
-    $scope.markers={
+        lat : 59.91,
+        lng : 10.75,
+        zoom: 4
+    };
+    var tempObj = {};
+    tempObj.lat = 59.91;
+    tempObj.lng = 10.75;
+    tempObj.enable=['click','touch'];
+    /*tempObj.layer="Options";*/
+    tempObj.icon= {
+                        type: 'div',
+                        iconSize: [25, 60],
+                        popupAnchor:  [0, -50],
+                        iconAnchor:   [10, 45],
+                        html: '<img class="map-user-marker" src="assets/images/user-image.jpg">'  
+                 }
+    tempObj.message='<div class="cn-wrapper opened-nav" id="cn-wrapper"><ul>   <li><a href="#"><img class="calling-icon-map" src="assets/images/map-icons/icon_call.png"></a></li> <li><a href="#"><img src="assets/images/map-icons/icon_contact_rollover.png"></a></li> <li><a href="#"><img src="assets/images/map-icons/icon_favorite.png"></a></li>  <li><a href="#"><img class="add-icon-map" src="assets/images/map-icons/icon_add.png"></a></li>  </ul></div>';
+    $scope.markers.push(tempObj)
+     var eventNameClick = 'leafletDirectiveMarker.myMap.click';
+     var eventNameTouch = 'leafletDirectiveMarker.myMap.touch';
+                $scope.$on(eventNameClick, function(event, args){
+                    
+
+                 var  wrapper = document.getElementById('cn-wrapper');
+                 classie.add(wrapper, 'opened-nav');
+
+                });
+                $scope.$on(eventNameTouch, function(event, args){
+                    
+
+                 var  wrapper = document.getElementById('cn-wrapper');
+                 classie.add(wrapper, 'opened-nav');
+
+                });
+    /*{
             osloMarker: {
                 lat: 59.91,
                 lng: 10.75,
@@ -16,6 +50,8 @@ angular.module('cbApp')
                 focus: true,
                 draggable: false
             }
-        }
+        }*/
+
+
     
   });
