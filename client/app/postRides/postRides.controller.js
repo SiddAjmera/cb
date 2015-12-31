b2c5ae1a940bd5cfb9fda2c5c80a7a2b60dd1e9c
unknown <962060@01HW507669.India.TCS.com>
2015-12-31 13:03:08 +0530

    post rides

--- client/app/postRides/postRides.controller.js
+++ client/app/postRides/postRides.controller.js
@@ -3,4 +3,26 @@
 angular.module('cbApp')
   .controller('PostRidesCtrl', function ($scope) {
     $scope.message = 'Hello';
+
+    $scope.leavingInJSON = ["5 MIN.",
+                                "10 MIN.",
+                                "15 MIN.",
+                                "20 MIN.",
+                                "25 MIN.",
+                                "30 MIN.",
+                                "35 MIN.",
+                                "40 MIN.",
+                                "45 MIN.",
+                                "50 MIN.",
+                                "55 MIN.",
+                                "60 MIN."
+                          	];
+
+    $scope.availableSeatsJSON = ["1",
+    						 "2",
+    						 "3",
+    						 "4",
+    						 "5",
+    						 "6"
+    						];
   });
