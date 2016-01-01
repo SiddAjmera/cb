dd0bc3ef94b2ba4a34b84699c31ec6ddc90d5f9c
Dess IOT <dess@Desss-MacBook-Pro.local>
2016-01-01 19:49:53 +0530

    location sampling

--- client/app/startSampling/startSampling.controller.js
+++ client/app/startSampling/startSampling.controller.js
@@ -5,8 +5,14 @@ angular.module('cbApp')
     $scope.message = 'Hello';
     $scope.buttonText="START SAMPLING";
 
-     $scope.defaults={minZoom:10, maxZoom:15,tap:true, tileLayer:"http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" }
-
+     $scope.defaults={minZoom:10, maxZoom:20,tap:true, tileLayer:"http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" }
+  $scope.center={
+        lat : 18.581904504725568,
+        lng : 73.68483066558838,
+        zoom: 15
+    };
+    $scope.setCenter=true;
+$scope.paths={};
     $scope.startOrStopSampling = function(value){
     	if(value == "START SAMPLING"){
     		$scope.buttonText="STOP SAMPLING";
@@ -26,9 +32,13 @@ angular.module('cbApp')
                else{
                 var pathArr=[];
                 storedlocations.TrackedLocations.forEach(function(obj){
-                        pathArr.push( { lat: obj.location.latitude, lng: obj.location.latitude.longitude })
+                        pathArr.push( { lat: obj.location.latitude, lng: obj.location.longitude })
                 })
-               $scope.center=pathArr[0];
+                if($scope.setCenter){
+                    $scope.center=pathArr[0];
+                    $scope.setCenter=false;
+                }
+               
                 $scope.paths={
                      p1: {
                 color: '#008000',
