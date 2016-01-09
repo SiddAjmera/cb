3d8aa5f858b4fcda65b8cd8637da99f7d632fad6
Ninad Mahajan <ninad.mahajan@tcs.com>
2016-01-09 14:26:11 +0530

    socket io

--- client/app/availableRides/availableRides.controller.js
+++ client/app/availableRides/availableRides.controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('cbApp')
-  .controller('AvailableRidesCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('AvailableRidesCtrl', ['$scope','socket',function ($scope,socket) {
+      $scope.message = 'Hello';
+      socket.syncUpdates('thing', []);
+    }]);
