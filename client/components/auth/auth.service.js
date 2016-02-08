58e44fab2790a2361993ed88b53d54400faa04e3
Dess IOT <dess@Desss-MacBook-Pro.local>
2016-02-08 11:59:25 +0530

    loggin failuer fix

--- client/components/auth/auth.service.js
+++ client/components/auth/auth.service.js
@@ -216,7 +216,7 @@ angular.module('cbApp')
       isLoggedInAsync: function(cb) {
         //changed 
         fetchUser().then(function(user){
-          if(user.hasOwnProperty('userId'))
+          if(user.hasOwnProperty('empId'))
             cb(true);
           else
             cb(false);
