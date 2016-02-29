0c1e9a0a9f0bfa27b63d6238e1217f8b4a4c3d8d
unknown <962060@01HW507669.India.TCS.com>
2016-02-29 19:31:09 +0530

    User Profile WIP

--- client/app/userProfile/userProfile.controller.js
+++ client/app/userProfile/userProfile.controller.js
@@ -3,18 +3,18 @@
 angular.module('cbApp')
   .controller('UserProfileCtrl', function ($scope, $state, Auth, $modal, cordovaUtil, $cordovaImagePicker, httpRequest, staticData) {
     $scope.message = 'Hello';
+    $scope.editableMode = false;
     
     Auth.getCurrentUser().
      then(function(data){
         $scope.user = data;
      });
 
+    $scope.leftButtonText = "EDIT";
+    $scope.rightButtonText = "LOGOUT";
 
     $scope.vehicleCapacityJSON = ["2","3","4","5","6"];
     $scope.officeAddressJSON = staticData.getTCSLocations();
-    $scope.editableMode = false;
-    $scope.editButtonText = "EDIT";
-    $scope.logoutButtonText = "LOG OUT"
     $scope.timeSlotJSON = [
                             {'start':'8:00 AM','end':'5:00 PM'},
                             {'start':'9:00 AM','end':'6:00 PM'},
@@ -23,18 +23,37 @@ angular.module('cbApp')
                             {'start':'12:00 AM','end':'9:00 PM'},                                
                           ];
 
-    $scope.changeMode = function(){
-      $scope.editableMode = !$scope.editableMode;
-      $scope.editButtonText = $scope.editButtonText=="EDIT"?"UPDATE":"EDIT";
-      $scope.logoutButtonText = $scope.logoutButtonText=="LOG OUT"?"CANCEL":"LOG OUT";
-    }
+    $scope.operation = function(buttonText){
+      if(buttonText == "EDIT"){
+        $scope.leftButtonText = "UPDATE";
+        $scope.rightButtonText = "CANCEL";
+        $scope.editableMode = true;
+        $scope.user.homeAddress = $scope.user.homeAddressLocation.display_address;
+      }
+      else if(buttonText == "LOGOUT"){
+        Auth.logout();
+        $state.go("login");
+      }
+      else if(buttonText == "UPDATE"){
+        // Update
+        alert("UPDATE Called");
+        saveDetails();
+        $scope.editableMode = false;
+      }
+      else if(buttonText == "CANCEL"){
+        $scope.leftButtonText = "EDIT";
+        $scope.rightButtonText = "LOGOUT";
+        $scope.editableMode = false;
+      }
+    };
+
     $scope.getLocation=function(){
           var modalInstance = $modal.open({
-          animation: true,
-          templateUrl: 'components/modal/modal.html',
-          controller: 'ModalCtrl',
-          size: 'sm'
-        });
+            animation: true,
+            templateUrl: 'components/modal/modal.html',
+            controller: 'ModalCtrl',
+            size: 'sm'
+          });
         
         modalInstance.result.then(function(option){
           if(option == "yes")
@@ -52,14 +71,14 @@ angular.module('cbApp')
       $(".home-page-menu-options").slideToggle(250);
     }
 
-    $scope.logout = function(){
+    /*$scope.logout = function(){
       if($scope.logoutButtonText=="CANCEL")
         $scope.changeMode();
       else{
         Auth.logout();
         $state.go("login")
       }
-    }
+    }*/
 
     $scope.getImageSaveContact = function() {       
             // Image picker will load images according to these settings
@@ -91,27 +110,40 @@ angular.module('cbApp')
 
 
     $scope.saveDetails=function () {
-      console.log("$scope.user",$scope.user);
       var obj = {};
-      obj.vehicle = $scope.user.vehicle;
-      obj.timeSlot = $scope.user.timeSlot;
-      obj.officeAddressLocation = $scope.user.officeAddress;
+      
+      obj.contactNo = $scope.user.contactNo;
+
       obj.homeAddressLocation = {};
       obj.homeAddressLocation.display_address = $scope.user.homeAddress.name;
       obj.homeAddressLocation.formatted_address = $scope.user.homeAddress.formatted_address;
       obj.homeAddressLocation.icon = $scope.user.homeAddress.icon;
       obj.homeAddressLocation.placeId = $scope.user.homeAddress.place_id;
-      obj.homeAddressLocation.location = []
+      obj.homeAddressLocation.location = [];
       obj.homeAddressLocation.location.push($scope.user.homeAddress.geometry.location.lng());
       obj.homeAddressLocation.location.push($scope.user.homeAddress.geometry.location.lat());
 
-        for(var i=0, len = $scope.user.homeAddress.address_components.length; i < len; i++) {
-            var ac = $scope.user.homeAddress.address_components[i];
-            console.log(ac);
-            if(ac.types.indexOf("administrative_area_level_2") >= 0) obj.city = ac.long_name;
-            if(ac.types.indexOf("administrative_area_level_1") >= 0) obj.state = ac.long_name;
-            if(ac.types.indexOf("postal_code") >= 0) obj.zipcode = ac.long_name;
-        }
+      obj.officeAddressLocation = $scope.user.officeAddress;
+
+      obj.vehicle = [];
+      var vehicle = {};
+      vehicle.vehicleLicenseNumber = $scope.user.vehicle[0].vehicleLicenseNumber;
+      vehicle.capacity = $scope.user.vehicle[0].capacity;
+      obj.vehicle.push(vehicle);
+
+      obj.shiftTimeIn = $scope.user.shiftTimeIn;
+
+      obj.shiftTimeout = $scope.user.shiftTimeout;
+
+      for(var i=0, len = $scope.user.homeAddress.address_components.length; i < len; i++) {
+          var ac = $scope.user.homeAddress.address_components[i];
+          console.log(ac);
+          if(ac.types.indexOf("administrative_area_level_2") >= 0) obj.city = ac.long_name;
+          if(ac.types.indexOf("administrative_area_level_1") >= 0) obj.state = ac.long_name;
+          if(ac.types.indexOf("postal_code") >= 0) obj.zipcode = ac.long_name;
+      }
+
+      console.log("Final Updated User Object : ", obj);
 
       var url = config.apis.signup + currentUser._id;
       httpRequest.put(url,obj)
