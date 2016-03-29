0cc74d28ce04fac11be8338f8cdf58e953f2536c
Siddharth Ajmera <siddharthajmeracs@hotmail.com>
2016-03-30 01:16:50 +0530

    Some other UI Enhancements

--- client/app/intro/intro.controller.js
+++ client/app/intro/intro.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('cbApp')
-  .controller('IntroCtrl', function ($scope) {
+  .controller('IntroCtrl', function ($scope, Auth, $state) {
     $scope.message = 'Hello';
+    $scope.goToLandingPage = function(){
+    	Auth.logout();
+        $state.go("signup");
+    };
   });
